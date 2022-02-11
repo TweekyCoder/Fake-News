3c5b5297be0b336fc03b02c8bc43aec60c63b80c
TweekyCoder <94781517+TweekyCoder@users.noreply.github.com>
2022-02-11 21:08:15 +0000

    Initial Commit

---
+++ navbar.js
@@ -0,0 +1,58 @@
+var nav = document.querySelector("nav");
+nav.innerHTML = "";
+
+var logo = document.createElement("div");
+logo.classList.add("logo");
+
+var linkAnchor = document.createElement("a");
+linkAnchor.classList.add("linkAnchor");
+linkAnchor.href = "index.html";
+
+var logoImg = document.createElement("img");
+logoImg.classList.add("logoImg");
+logoImg.src = "images/logo.jpeg"
+linkAnchor.appendChild(logoImg);
+
+logo.appendChild(linkAnchor);
+
+nav.appendChild(logo);
+
+var moreBtn = document.createElement("button");
+moreBtn.classList.add("moreBtn");
+moreBtn.innerHTML = '<i class="fas fa-bars"></i>';
+
+nav.appendChild(moreBtn);
+
+var more = document.createElement("div");
+more.classList.add("moreContainer");
+
+var moreTitle = document.createElement("div");
+moreTitle.classList.add("moreTitle");
+moreTitle.innerHTML = "Other Links";
+
+more.appendChild(moreTitle);
+
+//<i class="fas fa-fingerprint"></i>
+
+var linkNames = [ '<i class="fas fa-home"></i> Home','<i class="fas fa-fingerprint"></i>  Identification', '<i class="fas fa-video"></i> Example Videos', '&nbsp', '<i class="fas fa-list"></i> Credits'];
+var linkSrcs = ["index.html","identification.html", "vids.html", "identification.html", "credits.html"];
+
+for(i = 0; i < linkNames.length; i++) {
+	var link = document.createElement("div");
+	link.classList.add("moreLink");
+
+	var linkAnchor = document.createElement("a");
+	linkAnchor.classList.add("linkAnchor");
+	linkAnchor.href = linkSrcs[i];
+	linkAnchor.innerHTML = linkNames[i];
+
+	link.appendChild(linkAnchor);
+
+	more.appendChild(link);
+}
+
+nav.appendChild(more);
+
+document.querySelector(".moreBtn").addEventListener("click", function() {
+	document.querySelector(".moreContainer").classList.toggle("show");
+});
