1d29ac724f94e33f7779f8078a95f4fda5333868
TweekyCoder <canada-kid@outlook.com>
2022-02-13 18:51:33 +0000

    h

--- script.js
+++ script.js
@@ -2,6 +2,6 @@
     var link = document.querySelector("link[rel*='icon']") || document.createElement('link');
     link.type = 'image/x-icon';
     link.rel = 'shortcut icon';
-    link.href = 'https://fake-news-project.netlify.app/images/favicon.jfif';
+    link.href = 'https://fake-news-project.netlify.app/images/OffFavicon.jfif';
     document.getElementsByTagName('head')[0].appendChild(link);
 })();
