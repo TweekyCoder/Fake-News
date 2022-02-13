cab219c564d4841eec67fb8180fa001b86b4b11e
TweekyCoder <canada-kid@outlook.com>
2022-02-13 18:59:31 +0000

    s

--- script.js
+++
@@ -1,7 +0,0 @@
-(function() {
-    var link = document.querySelector("link[rel*='icon']") || document.createElement('link');
-    link.type = 'image/x-icon';
-    link.rel = 'shortcut icon';
-    link.href = 'https://fake-news-project.netlify.app/images/favicon.jpg';
-    document.getElementsByTagName('head')[0].appendChild(link);
-})();
